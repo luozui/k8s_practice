262a642a7783d6b9dcd0ff4a409a849a122d046d
luozui <329690971@qq.com>
2020-05-15 04:22:30 -0400

    first commit

---
+++ app1-server/view/article.go
@@ -0,0 +1,49 @@
+package view
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/app1-server/models"
+	"github.com/app1-server/dal/db"
+	"github.com/app1-server/dal/cache"
+)
+
+func AddArticle(c *gin.Context) {
+	var article models.Article
+	if err := c.ShouldBindJSON(&article); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	//article.Create()
+	c.JSON(http.StatusCreated, article)
+}
+
+func GetArticle(c *gin.Context) {
+	hostname, _ := os.Hostname()
+	strID := c.DefaultQuery("id", "1")
+	ID, _ := strconv.Atoi(strID)
+	c.Header("Access-Control-Allow-Origin", "*")
+	article, err := db.GetArticle(ID)
+	if err != nil {
+		resp := map[string]interface{} {
+			"title": "error",
+			"content": "error",
+			"view_cnt": 0,
+			"hostname": hostname,
+		}
+		c.JSON(500, resp)
+		return
+	}
+	viewCnt, err := redis.GetCntAndInc(fmt.Sprint("article_%d", ID))
+	resp := map[string]interface{} {
+		"title": article.Title,
+		"content": article.Content,
+		"view_cnt": viewCnt,
+		"hostname": hostname,
+	}
+	c.JSON(http.StatusOK, resp)
+}
