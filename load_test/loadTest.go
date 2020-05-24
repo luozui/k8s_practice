61df6de844dc0fb113721266a43922eabcab9491
luozui <329690971@qq.com>
2020-05-24 03:14:08 -0400

    load Test 负载测试 pudate

--- load_test/loadTest.go
+++ load_test/loadTest.go
@@ -16,37 +16,52 @@ type res struct {
 }
 
 func requestServer(url string, startTime int64) res {
-	resp, _ := http.Get(url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return res{fmt.Sprint(err), -1}
+	}
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return res{fmt.Sprint(err), -1}
+	}
 	var data map[string]interface{}
-	json.Unmarshal(body, &data)
+	if err := json.Unmarshal(body, &data); err != nil {
+		return res{fmt.Sprint(err), -1}
+	}
 	return res{data["hostname"].(string), time.Now().UnixNano() - startTime}
 }
 
-func work(url string, n, t int64) (int, int, int64, map[string]int) {
+func work(url string, n, t int64) (int, int, int64, map[string]int, map[string]int) {
 	ch := make(chan res, n)
 	for i := int64(0); i < n*t; i++ {
 		go func() {
 			ch <- requestServer(url, time.Now().UnixNano())
 		}()
 		time.Sleep(time.Microsecond * 1000000 / time.Duration(n))
+		if 100*(i+1)/(n*t) > 100*i/(n*t) {
+			fmt.Printf("进度: %d%% \r", 100*(i+1)/(n*t))
+		}
 	}
 	missCnt, successCnt := 0, 0
 	avgttl := int64(0)
-	hostCnt := make(map[string]int)
+	hostCnt, errCnt := make(map[string]int), make(map[string]int)
 	for i := int64(0); i < n*t; i++ {
 		req := <-ch
+		if 100*(i+1)/(n*t) > 100*i/(n*t) {
+			fmt.Printf("进度: %d%% \r", 100*(i+1)/(n*t))
+		}
 		if req.time < 0 {
 			missCnt++
+			errCnt[req.hostname]++
 		} else {
 			successCnt++
 			avgttl += req.time
+			hostCnt[req.hostname]++
 		}
-		hostCnt[req.hostname]++
 	}
 	avgttl /= int64(successCnt)
-	return missCnt, successCnt, avgttl, hostCnt
+	return missCnt, successCnt, avgttl, hostCnt, errCnt
 }
 
 func main() {
@@ -59,10 +74,14 @@ func main() {
 	fmt.Println("Duration:", os.Args[3], "s")
 	cnt, _ := strconv.ParseInt(os.Args[2], 10, 64)
 	t, _ := strconv.ParseInt(os.Args[3], 10, 64)
-	missCnt, successCnt, avgttl, hostCnt := work(os.Args[1], cnt, t)
-	fmt.Printf("-------\nmiss: %d, success: %d, 平均访问时间: %f ms\n", missCnt, successCnt, float64(avgttl)/1000000.0)
+	missCnt, successCnt, avgttl, hostCnt, errCnt := work(os.Args[1], cnt, t)
+	fmt.Printf("\n-------\nmiss: %d, success: %d, 平均访问时间: %f ms\n", missCnt, successCnt, float64(avgttl)/1000000.0)
 	fmt.Println("-------\n各个Pod处理请求数（负载均衡实验）：")
 	for k, v := range hostCnt {
+		fmt.Println(k, ":", v)
+	}
+	fmt.Println("-------\nErrors：")
+	for k, v := range errCnt {
 		fmt.Println(k, ":", v)
 	}
 }
